53a7d1eeebdebce51e958176853d648205d2ccac
enamazanu3-hub <enamazanu3@gmail.com>
2026-05-31 23:14:39 +0000

    Add files via upload

---
+++ AEA.py
@@ -0,0 +1,33 @@
+print("")
+
+def print_initials():  # A function to print initials
+
+# File: AEA.py
+# Description: Print out my initials.
+# Assignment Number:2
+# Name: Anthoinette Enam Azanu
+# STUDENT ID:2425404665
+# Email: 2425404665@live.gctu.edu.gh
+# Grader: Augustus
+# On my honor, Augustus Anthoinette, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+    print("   ...AEA")
+    print("")
+
+    print("   .......AAAA............EEEEEEEEEEEE............AAAA.........")
+    print("   .....AAAAAAAA..........EEEEEEEEEEEE..........AAAAAAAA.......")
+    print("   ....AAA.....AAA........EEE.................AAA......AAA.....")
+    print("   ...AAA......AAA........EEE.................AAA......AAA.....")
+    print("   ...AAA......AAA........EEEEEEEEEEEE........AAA......AAA.....")
+    print("   ...AAAAAAAAAAAA........EEEEEEEEEEEE........AAAAAAAAAAAA.....")
+    print("   ...AAAAAAAAAAAA........EEE.................AAAAAAAAAAAA.....")
+    print("   ...AAA......AAA........EEE.................AAA......AAA.....")
+    print("   ...AAA......AAA...**...EEEEEEEEEEEE...**...AAA......AAA...**")
+    print("   ...AAA.   ..AAA...**...EEEEEEEEEEEE...**...AAA......AAA...**")
+
+    print("")
+
+
+print_initials()
+print("")
